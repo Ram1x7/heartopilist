73c539ed5dc49b6eb592bf953da9d596b78f8625
Ram1x7 <saiyowa0517@gmail.com>
2026-06-23 00:23:35 +0900

    Create data-foods.js

---
+++ js/data-foods.js
@@ -0,0 +1,17 @@
+const foodsData = [
+  {
+    id: 1,
+    name: "田園サラダ",
+    image: "foods/001.PNG",
+    level: 1,
+    rarity: [true,true,true,true,true],
+    prices: [90,135,180,360,720],
+    materials: [
+      { image:"materials/tomato.png" },
+      { image:"materials/tomato.png" },
+      { image:"materials/egg.png" },
+      { image:"materials/fish_any.png" }
+    ],
+    hasMaster:true
+  }
+];
