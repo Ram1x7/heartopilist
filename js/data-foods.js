e21c5f445ab8e2b5b3cafb93b9ee5d9d16f532d0
Ram1x7 <saiyowa0517@gmail.com>
2026-06-23 00:24:32 +0900

    Update data-foods.js

--- js/data-foods.js
+++ js/data-foods.js
@@ -2,15 +2,15 @@ const foodsData = [
   {
     id: 1,
     name: "田園サラダ",
-    image: "foods/001.PNG",
+    image: "./images/foods/001.PNG",
     level: 1,
     rarity: [true,true,true,true,true],
     prices: [90,135,180,360,720],
     materials: [
-      { image:"materials/tomato.png" },
-      { image:"materials/tomato.png" },
-      { image:"materials/egg.png" },
-      { image:"materials/fish_any.png" }
+      { image:"./images/materials/tomato.png" },
+      { image:"./images/materials/tomato.png" },
+      { image:"./images/materials/egg.png" },
+      { image:"./images/materials/fish_any.png" }
     ],
     hasMaster:true
   }
