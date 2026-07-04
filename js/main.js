205f215056e682d53db34548d56d099ecbd45fc2
Ram1x7 <saiyowa0517@gmail.com>
2026-07-04 23:55:27 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,22 +22,22 @@ let authData =
 
 // 天気(毎日入力する) 
 const weatherData = {
- "2026-07-02": {
+ "2026-07-04": {
    "6-12":"晴れ",
    "12-18":"晴れ",
    "18-0":"晴れ",
    "0-6":"晴れ"
  },
-  "2026-07-04": {
+  "2026-07-05": {
    "6-12":"晴れ",
    "12-18":"晴れ",
    "18-0":"晴れ",
    "0-6":"晴れ"
  },
-   "2026-07-03": {
+   "2026-07-06": {
    "6-12":"晴れ",
    "12-18":"晴れ",
-   "18-0":"流星雨",
+   "18-0":"晴れ",
    "0-6":"晴れ"
  }
 };
@@ -1287,7 +1287,7 @@ document.getElementById("disclaimer").textContent =
   T("disclaimer","※本ツールは個人が制作した非公式のものです。ゲーム公式とは一切関係ありません。");
 
 document.getElementById("lastUpdate").textContent =
-  T("last_update_label","最終更新") + " 2026/07/02";
+  T("last_update_label","最終更新") + " 2026/07/04";
 
 // 言語切替時に動的コンテンツを再描画
 document.addEventListener("langchange", ()=>{
@@ -1296,7 +1296,7 @@ document.addEventListener("langchange", ()=>{
   document.getElementById("disclaimer").textContent =
     T("disclaimer","※本ツールは個人が制作した非公式のものです。ゲーム公式とは一切関係ありません。");
   document.getElementById("lastUpdate").textContent =
-    T("last_update_label","最終更新") + " 2026/07/02";
+    T("last_update_label","最終更新") + " 2026/07/04";
 
   // モーダル表示中なら翻訳を反映して再表示
   if(modal && modal.style.display === "block" && modal.dataset.currentCreature){
